900ba49eb2c6c58ad4f1db292e0998e652814140
Naimur Rahman <nevergiveupnr.tech@gmail.com>
2023-09-30 03:39:11 +0600

    half.l

---
+++ sample.c
@@ -0,0 +1,26 @@
+//Single
+/~ Multi lkjadlk
+snlafl
+lkshlkfa
+lkaslk ~/
+#include<stdio.h>
+vari main(){
+	vari a,b,c,d=a-b;
+	varf c="abc";
+	varc c=10; 
+	return 0;
+	c++;
+	c--;
+	c gr 5;
+	5 ne 4;
+if(a||b)
+if(!a)
+a+b;
+b$a;
+}
+
+varf add(vari x, vari y){
+}
+
+void sub(){
+}
